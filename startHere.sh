7c706214280cba59fd63638b25edf466b86637ac
Petar Deljanin <popos@komp.local>
2023-05-03 04:25:10 +0200

    Get this next time you run linux somewhere to help yourself.

--- startHere.sh
+++ startHere.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
-#install zsh ang wget
+#install zsh and wget
 sudo apt install zsh wget -y
 #install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -9,7 +9,7 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsM
 mkdir ~/.local/share/fonts
 unzip -o JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -f -v
-#make zsh the defualt shell
+#make zsh the default shell
 chsh -s $(which zsh)
 #move the theme to the correct folder
 cp agonster-edited.zsh-theme ~/.oh-my-zsh/custom/themes/agonster-edited.zsh-theme
@@ -17,13 +17,16 @@ cp agonster-edited.zsh-theme ~/.oh-my-zsh/custom/themes/agonster-edited.zsh-them
 #echo 'export ZSH_THEME="agonster-edited"' | tee -a ~/.zshrc
 sed -i 's/^export ZSH_THEME=.*/export ZSH_THEME="agonster-edited"/' ~/.zshrc
 
-#Creates the dircolors file
+#Create the dircolors file
 dircolors -p > ~/.dircolors
-#Changes the green backgroudn to transparent ffs
+#Change the green background to transparent ffs
 sed -i 'tw/;42/;01/' ~/.dircolors #STICKY_OTHER_WRITABLE 30;01 # dir that is sticky and other-writable (+t,o+w)
 sed -i 'ow/;42/;01/' ~/.dircolors#OTHER_WRITABLE 34;01 # dir that is other-writable (o+w) and not sticky
 sed -i 'st/;42/;01/' ~/.dircolors#STICKY 37;01 # dir with the sticky bit set (+t) and not other-writable
 #      'tw/OLD_STRING/NEW_STRING/'
+
+#reload everything
+source ~/.zshrc
 
 #cleanup
 rm JetBrainsMono.zip
